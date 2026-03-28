dacad2a96f2df9a8645ca0ac5e30dd1bd5e8c4c0
Amey Shaligram <ameyrshaligram@gmail.com>
2026-03-28 11:38:32 +0100

    Improve SettingsModal layout to group settings

--- src/components/SettingsModal.jsx
+++ src/components/SettingsModal.jsx
@@ -21,109 +21,115 @@ export default function SettingsModal({ settings, saveSettings, ollamaModels, cl
           <span>Settings</span>
           <IconButton icon={IconButtonEnum.CLOSE} onClick={closeModal} />
         </div>
-        <div className='settings-ollama'>
-          <span>Select Model: </span>
-          <select value={ollamaModel} onChange={(e) => setOllamaModel(e.target.value)}>
-            {
-              ollamaModels.map(model => (
-                <option key={model.model} value={model.model}>{model.model}</option>
-              ))
-            }
-          </select>
-        </div>
-        <div className='settings-ollama'>
-          <span>Select Summary Model: </span>
-          <select value={summaryModel} onChange={(e) => setSummaryModel(e.target.value)}>
-            {
-              ollamaModels.map(model => (
-                <option key={model.model} value={model.model}>{model.model}</option>
-              ))
-            }
-          </select>
-        </div>
-        <textarea
-          className='settings-system-instructions'
-          rows={10}
-          value={systemInstructions}
-          onChange={(e) => setSystemInstructions(e.target.value)}
-          placeholder='System Instructions'
-        />
-        <textarea
-          className='settings-system-instructions'
-          rows={10}
-          value={summaryInstructions}
-          onChange={(e) => setSummaryInstructions(e.target.value)}
-          placeholder='Summary Instructions'
-        />
-        <div className='settings-ollama'>
-          <span>temperature: </span>
-          <input
-            type="number"
-            value={options.temperature}
-            onChange={(e) => setOptions(
-              { ...options, temperature: parseFloat(e.target.value) }
-            )}
+        <div className='settings-group'>
+          <h3 className='settings-group-title'>System Prompts</h3>
+          <div className='settings-ollama'>
+            <span>Select Roleplay Model: </span>
+            <select value={ollamaModel} onChange={(e) => setOllamaModel(e.target.value)}>
+              {
+                ollamaModels.map(model => (
+                  <option key={model.model} value={model.model}>{model.model}</option>
+                ))
+              }
+            </select>
+          </div>
+          <textarea
+            className='settings-system-instructions'
+            rows={10}
+            value={systemInstructions}
+            onChange={(e) => setSystemInstructions(e.target.value)}
+            placeholder='System Instructions'
+          />
+          <div className='settings-ollama'>
+            <span>Select Summary Model: </span>
+            <select value={summaryModel} onChange={(e) => setSummaryModel(e.target.value)}>
+              {
+                ollamaModels.map(model => (
+                  <option key={model.model} value={model.model}>{model.model}</option>
+                ))
+              }
+            </select>
+          </div>
+          <textarea
+            className='settings-system-instructions'
+            rows={10}
+            value={summaryInstructions}
+            onChange={(e) => setSummaryInstructions(e.target.value)}
+            placeholder='Summary Instructions'
           />
         </div>
-        <div className='settings-ollama'>
-          <span>top-p: </span>
-          <input
-            type="number"
-            value={options.top_p}
-            onChange={(e) => setOptions(
-              { ...options, top_p: parseFloat(e.target.value) }
-            )}
-          />
-        </div>
-        <div className='settings-ollama'>
-          <span>top-k: </span>
-          <input
-            type="number"
-            value={options.top_k}
-            onChange={(e) => setOptions(
-              { ...options, top_k: parseInt(e.target.value) }
-            )}
-          />
-        </div>
-        <div className='settings-ollama'>
-          <span>num_predict: </span>
-          <input
-            type="number"
-            value={options.num_predict}
-            onChange={(e) => setOptions(
-              { ...options, num_predict: parseInt(e.target.value) }
-            )}
-          />
-        </div>
-        <div className='settings-ollama'>
-          <span>repeat_penalty: </span>
-          <input
-            type="number"
-            value={options.repeat_penalty}
-            onChange={(e) => setOptions(
-              { ...options, repeat_penalty: parseFloat(e.target.value) }
-            )}
-          />
-        </div>
-        <div className='settings-ollama'>
-          <span>repeat_last_n: </span>
-          <input
-            type="number"
-            value={options.repeat_last_n}
-            onChange={(e) => setOptions(
-              { ...options, repeat_last_n: parseInt(e.target.value) }
-            )}
-          />
-        </div>
-        <div className='settings-ollama'>
-          <span>num_ctx: </span>
-          <input
-            type="number"
-            value={options.num_ctx}
-            onChange={(e) => setOptions(
-              { ...options, num_ctx: parseInt(e.target.value) }
-            )}
-          />
+        <div className='settings-group'>
+          <h3 className='settings-group-title'>Advanced Parameters</h3>
+          <div className='settings-ollama'>
+            <span>temperature: </span>
+            <input
+              type="number"
+              value={options.temperature}
+              onChange={(e) => setOptions(
+                { ...options, temperature: parseFloat(e.target.value) }
+              )}
+            />
+          </div>
+          <div className='settings-ollama'>
+            <span>top-p: </span>
+            <input
+              type="number"
+              value={options.top_p}
+              onChange={(e) => setOptions(
+                { ...options, top_p: parseFloat(e.target.value) }
+              )}
+            />
+          </div>
+          <div className='settings-ollama'>
+            <span>top-k: </span>
+            <input
+              type="number"
+              value={options.top_k}
+              onChange={(e) => setOptions(
+                { ...options, top_k: parseInt(e.target.value) }
+              )}
+            />
+          </div>
+          <div className='settings-ollama'>
+            <span>num_predict: </span>
+            <input
+              type="number"
+              value={options.num_predict}
+              onChange={(e) => setOptions(
+                { ...options, num_predict: parseInt(e.target.value) }
+              )}
+            />
+          </div>
+          <div className='settings-ollama'>
+            <span>repeat_penalty: </span>
+            <input
+              type="number"
+              value={options.repeat_penalty}
+              onChange={(e) => setOptions(
+                { ...options, repeat_penalty: parseFloat(e.target.value) }
+              )}
+            />
+          </div>
+          <div className='settings-ollama'>
+            <span>repeat_last_n: </span>
+            <input
+              type="number"
+              value={options.repeat_last_n}
+              onChange={(e) => setOptions(
+                { ...options, repeat_last_n: parseInt(e.target.value) }
+              )}
+            />
+          </div>
+          <div className='settings-ollama'>
+            <span>num_ctx: </span>
+            <input
+              type="number"
+              value={options.num_ctx}
+              onChange={(e) => setOptions(
+                { ...options, num_ctx: parseInt(e.target.value) }
+              )}
+            />
+          </div>
         </div>
         <button className='settings-save-button' onClick={handleSave}>Save</button>
       </div>
